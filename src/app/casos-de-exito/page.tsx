4683c075db9127cbd2ebb9908425231f778c1395
eSerranoRosete <eduardo.serrano@dovela.com>
2024-10-22 19:19:01 -0600

    Refactor Page component to improve layout and responsiveness

--- src/app/casos-de-exito/page.tsx
+++ src/app/casos-de-exito/page.tsx
@@ -11,13 +11,13 @@ export default function Page() {
   return (
     <>
       <NavBar />
-      <PageSection className="pt-14">
+      <PageSection className="pt-14 mb-0">
         <Container className="rounded-xl relative overflow-hidden">
           <img
             src="/Office_Workers_Laughing_original_548606.jpg"
             className="absolute top-0 left-0 w-full h-full object-cover object-center -z-10"
           />
-          <div className="w-full bg-gradient-to-t from-black/70 text-white to-transparent h-screen md:h-[60vh] px-10 pb-20 flex flex-col gap-2 items-center justify-end">
+          <div className="w-full bg-gradient-to-t from-black/70 text-white to-transparent h-[80vh] md:h-[60vh] px-10 pb-20 flex flex-col gap-2 items-center justify-end">
             <div className="flex flex-col gap-6 w-full">
               <h1 className="text-2xl md:text-5xl text-center font-semibold md:max-w-4xl md:m-auto">
                 Descubre cómo hemos ayudado a nuestros clientes a alcanzar el
